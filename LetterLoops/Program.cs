b104a7954029bcf0a830999b2800d81ba9011edc
Wayne Collier <ke4tri@gmail.com>
2019-02-27 14:18:26 -0600

    reading each letter individually annd printing out

--- LetterLoops/Program.cs
+++ LetterLoops/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LetterLoops
 {
@@ -6,8 +7,36 @@ namespace LetterLoops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            List<string> letters = new List<string>();
+
+            Console.WriteLine("Enter some random letters");
+
+            var input = Console.ReadLine();
+
+            char[] characters = input.ToCharArray();
+            foreach (char c in characters)
+            {
+                //Label1.Text += c.ToString() + " ";
+                Console.WriteLine(c.ToString());
+                Console.ReadLine();
+            }
+
+            //   int i,j;
+            //   int n=5;
+
+            //for(i=1;i<=n;i++)
+            //{
+            //    for(j=1;j<=i;j++)
+            //    {
+            //        Console.Write((char)(i+64));
+
+            //    }
+
+            //    Console.WriteLine("");
+            //}
+            //   Console.ReadLine();
         }
+          
+        
     }
 }
