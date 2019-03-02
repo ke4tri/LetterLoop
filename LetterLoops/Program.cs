914c714cfc952d8df00825ba9e6b391f4fcd189f
Wayne Collier <ke4tri@gmail.com>
2019-03-01 19:08:34 -0600

    completed

--- LetterLoops/Program.cs
+++ LetterLoops/Program.cs
@@ -13,23 +13,27 @@ namespace LetterLoops
             List<string> let = new List<string>();
 
             Console.WriteLine("Enter some random letters");
-
-           
-
             string input = Console.ReadLine();
-
             char[] charArray = input.ToCharArray();
 
+            
             for (int i = 0; i < input.Length; i++)
             {
                 int multipler = i;
                 string inputString = input[i].ToString();
                 string inputMulti = new string(input[i], multipler);
-                string build = inputString.ToUpper() + ( input[i] + inputMulti + "-");
-                // Console.Write((input[i] * i) + "-");
-                Console.Write(build);
-                
-                
+                string build = inputString.ToUpper() + ( input[i] + inputMulti) + "-";
+
+                if (i == input.Length -1)
+                {
+                    Console.Write(build.TrimEnd('-'));
+                }
+                else
+                {
+                    Console.Write(build);
+                }
+
+
             }
             Console.ReadLine();
             
@@ -38,6 +42,11 @@ namespace LetterLoops
        
     }
 }
+
+                
+                
+           
+
 
 
 
